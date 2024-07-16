fb1611b18004b9da5848c3f0f956dc40401430a4
Moein Salari <moeinsalar7@gmail.com>
2024-07-16 16:40:44 +0330

    chore: back to home site title

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -2,6 +2,7 @@
 
 import { PATHS } from '@/common/constants/PATHS';
 import clsx from 'clsx';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Toggle } from '../ui/Toggle';
 import { useNavigation } from './useNavigation';
@@ -13,16 +14,18 @@ export const Navigation = () => {
 
   return (
     <nav className='flex items-center justify-between'>
-      <h1
-        className={clsx(
-          'text-2xl font-bold transition-all duration-200',
-          isPostDetailPage
-            ? 'gradient-text'
-            : 'text-slate-700 dark:text-secondary'
-        )}
-      >
-        overreacted
-      </h1>
+      <Link href={PATHS.HOME}>
+        <h1
+          className={clsx(
+            'text-2xl font-bold transition-all duration-200',
+            isPostDetailPage
+              ? 'gradient-text'
+              : 'text-slate-700 dark:text-secondary'
+          )}
+        >
+          overreacted
+        </h1>
+      </Link>
       <Toggle
         checked={isDark}
         label={isDark ? 'Dark' : 'Light'}
